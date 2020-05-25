8bbd8ac789db1ef3c23c0c9663b08c497cea0fda
Emmanuel Barturen <emmanuelbarturen@gmail.com>
2020-05-24 19:02:27 -0500

    splash screeen

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+export default class LoginScreen extends Component {
+    render() {
+        return <Text style={{textAlign: 'center', marginTop: 200, fontFamily: 'Poppins-Black'}}>Cuycitos App y maz naha</Text>;
+    }
+}
+
