2d5171128e633b062192129c1bda8a082c858d60
Emmanuel Barturen <emmanuelbarturen@gmail.com>
2020-05-25 20:38:22 -0500

    conect socket

--- App.js
+++ App.js
@@ -1,114 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {Component} from 'react';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+import io from 'socket.io-client';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            chatMessage: '',
+            chatMessages: [],
+        };
+    }
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+    componentDidMount() {
+        this.socket = io('http://192.168.1.3:3000', {
+            forceNew: true
+        });
+        this.socket.on('connect', () => console.log('Connection'));
+        this.socket.on('chat message', msg => {
+            this.setState({chatMessages: [...this.state.chatMessages, msg]});
+        });
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+    }
+
+    submitChatMessage() {
+        this.socket.emit('chat message', this.state.chatMessage);
+        this.setState({chatMessage: ''});
+    }
+
+    render() {
+        const chatMessages = this.state.chatMessages.map(chatMessage => (
+            <Text key={chatMessage}>{chatMessage}</Text>
+        ));
+
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    style={{height: 40, borderWidth: 2}}
+                    autoCorrect={false}
+                    value={this.state.chatMessage}
+                    onSubmitEditing={() => this.submitChatMessage()}
+                    onChangeText={chatMessage => {
+                        this.setState({chatMessage});
+                    }}
+                />
+                {chatMessages}
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+        );
+    }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+    },
 });
-
-export default App;
--- README.md
+++ README.md
@@ -14,4 +14,9 @@ Cada funcionalidad está en una rama
 - GraphQL
 - Persistencia Local (Redux?)
 - Obtener ubicación por GPS
-- Comunicación en tiempo real (Socket.io?)
+- Comunicación en tiempo real
+    - El device donde corre la app debe estar en la misma red WIFI que la PC donde se ejecute el server
+    - instalar de modo globarl nodemon `npm i -g nodemon`
+    - En la carpeta server levantar express `nodemon main.js`
+    - En App.js (React Native), cambiar el ip por el ip de la red local WIFI. (ipconfig en windows)
+    - 
