315681f1e62b3dddb706ed1aaa23b791daddc78a
Emmanuel Barturen <emmanuelbarturen@gmail.com>
2020-05-29 20:05:52 -0500

    add custom toast

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import React, {Component} from 'react';
-import {View, Text, Button, Alert} from 'react-native';
-import Toast from 'react-native-simple-toast';
+import {View, Text, Button, Alert, StyleSheet, TouchableOpacity} from 'react-native';
+import SimpleToast from 'react-native-simple-toast';
+import CustomToast from 'react-native-custom-toast';
 
 export default class App extends Component {
+
+    constructor() {
+        super();
+    }
+
+    showDefaultToast() {
+        this.refs.defaultToast.showToast('Default Toast...');
+    }
 
     createTwoButtonAlert = () =>
         Alert.alert(
@@ -14,7 +23,7 @@ export default class App extends Component {
                     onPress: () => console.log("Cancel Pressed"),
                     style: "cancel"
                 },
-                {text: "OK", onPress: () => Toast.show("OK Pressed")}
+                {text: "OK", onPress: () => SimpleToast.show("OK Pressed")}
             ],
             {cancelable: false}
         );
@@ -26,33 +35,33 @@ export default class App extends Component {
             [
                 {
                     text: "Ask me later",
-                    onPress: () =>  console.log("Ask me later pressed")
+                    onPress: () => console.log("Ask me later pressed")
                 },
                 {
                     text: "Cancel",
                     onPress: () => console.log("Cancel Pressed"),
                     style: "cancel"
                 },
-                {text: "OK", onPress: () => Toast.show("OK Pressed")}
+                {text: "OK", onPress: () => SimpleToast.show("OK Pressed")}
             ],
             {cancelable: false}
         );
 
     showToast(message) {
         if (message.length > 16) {
-            Toast.show(message, Toast.LONG);
+            SimpleToast.show(message, SimpleToast.LONG);
         } else {
-            Toast.show(message);
+            SimpleToast.show(message);
         }
     }
 
     render() {
         return <View>
             <Text>TOAST</Text>
-            <Button onPress={() => this.showToast('Mensaje Corto')} title="Toast Corto"/>
+            <Button onPress={() => this.showToast('Mensaje Corto')} title="SimpleToast Corto"/>
             <Button
                 onPress={() => this.showToast('Este es un mensaje mucho más largo por lo que se muestra en un toast extenso')}
-                title="Toast Largo"/>
+                title="SimpleToast Largo"/>
             <Text>************************************************************</Text>
             <Text>Confirmation</Text>
             <Button title={"Botón confirmación"}
@@ -62,7 +71,37 @@ export default class App extends Component {
                 onPress={this.createThreeButtonAlert}
             />
             <Text>************************************************************</Text>
+            <TouchableOpacity onPress={this.showDefaultToast.bind(this)} activeOpacity={0.8}
+                              style={styles.showToastBtn}>
+                <Text style={styles.btnText}>Toast Personalizado</Text>
+            </TouchableOpacity>
+            <CustomToast ref="defaultToast"  backgroundColor="#28a745"  />
         </View>;
     }
 
 }
+const styles = StyleSheet.create(
+    {
+        container:
+            {
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+            },
+
+        showToastBtn:
+            {
+                padding: 10,
+                backgroundColor: 'rgba(0,0,0,0.5)',
+                alignSelf: 'stretch',
+                marginHorizontal: 25,
+                marginVertical: 10
+            },
+
+        btnText:
+            {
+                textAlign: 'center',
+                color: 'white',
+                fontSize: 18
+            }
+    });
--- README.md
+++ README.md
@@ -7,6 +7,11 @@ Mensajes del sistema hacia el usuario. En este ejemplo vamosa encontrar
 ### Docs
 Toast
 https://www.npmjs.com/package/react-native-simple-toast
+https://github.com/ranveerching/react-native-custom-toast
 
 Alerts
 https://reactnative.dev/docs/alert
+
+
+Alternaticas no implementadas en este ejemplo
+https://medium.com/@dabit3/creating-a-custom-toast-module-for-react-native-770fd1c0dcf5
