995cf3c18438fb6dc82705a8ce1e50d7c0bb0763
Emmanuel Barturen <emmanuelbarturen@gmail.com>
2020-06-01 22:31:32 -0500

    implementación del mapa

--- App.js
+++ App.js
@@ -1,16 +1,61 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        height: 400,
+        width: 400,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+});
+
 
 export default class App extends Component {
+    state = {
+        lat: 37.78825,
+        lng: -122.4324
+    };
+
     constructor(props) {
         super(props);
-        Geolocation.getCurrentPosition(info => console.log('obteniendo información del GPS', info))
+
+
+    }
+
+    autoLocation() {
+        Geolocation.getCurrentPosition(info => {
+            console.log('obteniendo información del GPS', info);
+            this.setState({lat: info.coords.latitude, lng: info.coords.longitude})
+        })
     }
 
     render() {
         return (<View>
-            <Text>GPS:</Text>
+            <View style={styles.container}>
+                <MapView
+                    provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                    style={styles.map}
+                    region={{
+                        latitude: this.state.lat,
+                        longitude: this.state.lng,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,
+                    }}
+                >
+                </MapView>
+
+            </View>
+            <Text style={{textAlign: 'center', marginTop: 450, marginBottom: 20}}>GPS Latitud:{this.state.lat},
+                Longitud:{this.state.lng}</Text>
+            <Button onPress={() => this.autoLocation()} title={'AutoLocation'}/>
+
         </View>);
     }
 }
--- README.md
+++ README.md
@@ -3,4 +3,12 @@ docs
 https://reactnative.dev/docs/geolocation.html#:~:text=Geolocation%20is%20enabled%20by%20default,'Capabilities'%20tab%20in%20Xcode.
 
 # Maps
-
+### instalación
+https://github.com/react-native-community/react-native-maps/blob/master/docs/installation.md
+### documentación
+https://github.com/react-native-community/react-native-geolocation
+### Config
+Poner el Api Key de google en el manifest `android/app/src/main/AndroidManifest.xml`
+```bash
+<meta-data android:name="com.google.android.geo.API_KEY" android:value="API_KEY"/>
+```
